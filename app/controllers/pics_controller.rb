cf5fffa862c2308cbc1dc5454ea0a594e43d3a25
Gouri A <vagrant@precise32.(none)>
2014-02-22 22:13:03 +0000

    Hook the form up

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,13 +3,28 @@ class PicsController < ApplicationController
 
 	def index
 	
-@pics = Pic.all
+	@pics = Pic.all
 
 
+end
 
+def new
 
+@pic = Pic.new
 
-	end
+end
+
+def create
+Pic.create ( pic_pararms )
+redirect_to pics_path
+end
+
+private 
+
+def pic_pararms
+	params.require(:pic).permit(:title, :subtitle, :message)
+
+end
 
 
 end
